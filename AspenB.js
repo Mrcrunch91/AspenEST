cca521717672e4d38d990a5cb3e16e3bb1cef31d
Mrcrunch91 <alterac91@yahoo.com>
2017-01-15 03:43:20 -0500

    Small changes.

--- AspenB.js
+++ AspenB.js
@@ -210,11 +210,11 @@ function bamGraph () {
   $(document).ready(function() {
 
    var docHeight = $(window).height();
-   var footerHeight = $('#theFooter').height();
-   var footerTop = $('#theFooter').position().top + footerHeight;
+   var footerHeight = $("#theFooter").height();
+   var footerTop = $("#theFooter").position().top + footerHeight;
 
    if (footerTop < docHeight) {
-    $('#footer').css('margin-top', 10+ (docHeight - footerTop) + 'px');
+    $("#theFooter").css('margin-top', 10+ (docHeight - footerTop) + 'px');
    }
   });
      
